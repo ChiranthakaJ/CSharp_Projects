867c857620c3f1ac52c17032de06891cfa7c3fa7
chiranthakaj <chiransj@gmail.com>
2020-11-03 00:02:11 +0530

    Second Commit

--- First_Project_DB_App_Config/First_Project_DB_App_Config/Form1.cs
+++ First_Project_DB_App_Config/First_Project_DB_App_Config/Form1.cs
@@ -10,11 +10,36 @@ using System.Windows.Forms;
 
 namespace First_Project_DB_App_Config
 {
-    public partial class Form1 : Form
+    public partial class frmDBConnectionForm : Form
     {
-        public Form1()
+        public frmDBConnectionForm()
         {
             InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
--- First_Project_DB_App_Config/First_Project_DB_App_Config/Program.cs
+++ First_Project_DB_App_Config/First_Project_DB_App_Config/Program.cs
@@ -16,7 +16,7 @@ namespace First_Project_DB_App_Config
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmDBConnectionForm());
         }
     }
 }
