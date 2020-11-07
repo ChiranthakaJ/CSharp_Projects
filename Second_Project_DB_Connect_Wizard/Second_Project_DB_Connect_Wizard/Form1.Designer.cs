65f57a48d858b8847aa89777dab88a8028e6cb62
chiranthakaj <chiransj@gmail.com>
2020-11-08 00:26:09 +0530

    First Commit

---
+++ Second_Project_DB_Connect_Wizard/Second_Project_DB_Connect_Wizard/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace First_Project_DB_App_Config
+{
+    partial class frmDBConnectionForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lbIndexNo = new System.Windows.Forms.ListBox();
+            this.lbFirstName = new System.Windows.Forms.ListBox();
+            this.lbLastName = new System.Windows.Forms.ListBox();
+            this.lbAddress = new System.Windows.Forms.ListBox();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(43, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Index No";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(203, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "First Name";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(371, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 18);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Last Name";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(506, 24);
+            this.label4.MaximumSize = new System.Drawing.Size(200, 20);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(130, 18);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Residential Address";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // lbIndexNo
+            // 
+            this.lbIndexNo.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbIndexNo.FormattingEnabled = true;
+            this.lbIndexNo.ItemHeight = 15;
+            this.lbIndexNo.Location = new System.Drawing.Point(15, 56);
+            this.lbIndexNo.Name = "lbIndexNo";
+            this.lbIndexNo.Size = new System.Drawing.Size(120, 124);
+            this.lbIndexNo.TabIndex = 5;
+            // 
+            // lbFirstName
+            // 
+            this.lbFirstName.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbFirstName.FormattingEnabled = true;
+            this.lbFirstName.ItemHeight = 15;
+            this.lbFirstName.Location = new System.Drawing.Point(180, 56);
+            this.lbFirstName.Name = "lbFirstName";
+            this.lbFirstName.Size = new System.Drawing.Size(120, 124);
+            this.lbFirstName.TabIndex = 6;
+            // 
+            // lbLastName
+            // 
+            this.lbLastName.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbLastName.FormattingEnabled = true;
+            this.lbLastName.ItemHeight = 15;
+            this.lbLastName.Location = new System.Drawing.Point(345, 56);
+            this.lbLastName.Name = "lbLastName";
+            this.lbLastName.Size = new System.Drawing.Size(120, 124);
+            this.lbLastName.TabIndex = 7;
+            // 
+            // lbAddress
+            // 
+            this.lbAddress.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbAddress.FormattingEnabled = true;
+            this.lbAddress.ItemHeight = 15;
+            this.lbAddress.Location = new System.Drawing.Point(510, 56);
+            this.lbAddress.Name = "lbAddress";
+            this.lbAddress.Size = new System.Drawing.Size(120, 124);
+            this.lbAddress.TabIndex = 8;
+            // 
+            // btnShow
+            // 
+            this.btnShow.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnShow.Location = new System.Drawing.Point(270, 220);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(114, 34);
+            this.btnShow.TabIndex = 9;
+            this.btnShow.Text = "Show!";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // frmDBConnectionForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(681, 279);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.lbAddress);
+            this.Controls.Add(this.lbLastName);
+            this.Controls.Add(this.lbFirstName);
+            this.Controls.Add(this.lbIndexNo);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmDBConnectionForm";
+            this.Text = " ";
+            this.Load += new System.EventHandler(this.frmDBConnectionForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox lbIndexNo;
+        private System.Windows.Forms.ListBox lbFirstName;
+        private System.Windows.Forms.ListBox lbLastName;
+        private System.Windows.Forms.ListBox lbAddress;
+        private System.Windows.Forms.Button btnShow;
+    }
+}
+
---
+++ Second_Project_DB_Connect_Wizard/Second_Project_DB_Connect_Wizard/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace First_Project_DB_App_Config
+{
+    public partial class frmDBConnectionForm : Form
+    {
+        SqlConnection SqlConn;
+        SqlDataReader SqlDr;
+        SqlCommand SqlCommand;
+
+        String ConnectionString;
+
+        public frmDBConnectionForm()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+
+            ConnectionString = "Data Source=CHIRAN-PC;Initial Catalog=FirstDatabaseProject;Integrated Security=True";
+
+        try
+            {
+                SqlConn = new SqlConnection(ConnectionString);
+                SqlConn.Open();
+                SqlCommand = new SqlCommand("SELECT * FROM PersonalDetails", SqlConn);
+                SqlDr = SqlCommand.ExecuteReader();
+
+                while (SqlDr.Read())
+                {
+                    lbIndexNo.Items.Add(SqlDr[0].ToString());
+                    lbFirstName.Items.Add(SqlDr[1].ToString());
+                    lbLastName.Items.Add(SqlDr[2].ToString());
+                    lbAddress.Items.Add(SqlDr[3].ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                SqlConn.Close();
+            }
+        }
+
+        private void frmDBConnectionForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
